0b029b2a230f3872a8368f0cc4731b71a51b5f13
Andrea Abbou <andrea.abbou@gmail.com>
2021-08-10 14:57:48 +0200

    02 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_091617) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_210_810_091_617) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.text 'details'
+    t.boolean 'completed', default: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
